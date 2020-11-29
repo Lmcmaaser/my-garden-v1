a7ed843dea431787e879e8522b8385c9ddac2a70
Lauren Maaser <lmcmaaser@gmail.com>
2020-11-28 17:22:26 -0700

    first commit

--- src/index.js
+++ src/index.js
@@ -4,12 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+/*Note: document.getElementById('root') achieves the same result as document.querySelector('#root').
+The querySelector method works with any CSS selector and getElementById only works with ids.*/
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))

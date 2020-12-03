2594d399e6d43eed42c0ad6281ce15d4dcb34b3a
Lauren Maaser <lmcmaaser@gmail.com>
2020-12-03 16:37:29 -0700

    sign up component

---
+++ src/AboutPage/AboutPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './AboutPage.css';
+
+export default class AboutPages extends Component {
+    render() {
+        return(
+            <div className="about">
+            </div>
+        )
+    }
+}
